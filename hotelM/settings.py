7c43d31c41fe5bf2d43b5b6947cd42f17c5fec28
minnu1306 <sarahjacob98@gmail.com>
2020-08-31 05:59:40 +0530

    second Commit

--- hotelM/settings.py
+++ hotelM/settings.py
@@ -119,8 +119,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-import django_heroku
-django_heroku.settings(locals())
+
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
